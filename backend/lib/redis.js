7082558d8ceda9cc19df4baa73d78a7bd25b9520
killingcammand <154828355+killingcammand@users.noreply.github.com>
2025-07-25 12:23:49 +0530

    1st commit

---
+++ backend/lib/redis.js
@@ -0,0 +1,10 @@
+import Redis from "ioredis";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+export const redis= new Redis(process.env.UPSTASH_REDIS_URL);
+// await client.set('foo', 'bar');
+
+
